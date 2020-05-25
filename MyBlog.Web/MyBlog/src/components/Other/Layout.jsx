441320f3a8e55f69120cddeed3df422fcd2d6adf
mrkomilfo <43483871+mrkomilfo@users.noreply.github.com>
2020-05-25 16:39:24 +0300

    add menu collapsing

--- MyBlog.Web/MyBlog/src/components/Other/Layout.jsx
+++ MyBlog.Web/MyBlog/src/components/Other/Layout.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import NavMenu from './NavMenu';
 
+import './Layout.css';
+
 export default class Layout extends Component {
   render () {
     return ( 
-      <div>
+      <>
         <NavMenu />
         <main>
           {this.props.children}
         </main>
-      </div>
+      </>
     );
   }
 }
