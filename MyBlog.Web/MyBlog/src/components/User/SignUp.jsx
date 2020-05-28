c1d3150c7e57c5ebed7b2df990c765c78765beb1
mrkomilfo <43483871+mrkomilfo@users.noreply.github.com>
2020-05-28 19:33:12 +0300

    add signUp form

---
+++ MyBlog.Web/MyBlog/src/components/User/SignUp.jsx
@@ -0,0 +1,191 @@
+import React, { Component } from 'react';
+import Alert from '../Common/Alert';
+
+import Button from '../Common/Button';
+
+import './SignUp.css';
+
+export default class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            error: false,
+            errorMessage: '',
+
+            name: '', 
+            email: '',
+            login: '', 
+            password: '', 
+            passwordConfirm: '',
+            formErrors: { 
+                name: '', 
+                email: '',
+                login: '', 
+                password: '',
+                passwordConfirm: '' 
+            },
+            formValid: false, 
+            nameValid: false, 
+            emailValid: true, 
+            loginValid: false, 
+            passwordValid: false, 
+            passwordConfirmValid: false,
+        };
+        this.handleinputChange = this.handleinputChange.bind(this);
+        this.validateField = this.validateField.bind(this);
+        this.register = this.register.bind(this);
+    }
+
+    handleinputChange(event) {
+        const target = event.target;
+        const name = target.name;
+        let value = target.value;
+
+        this.setState({
+          [name]: value}, 
+          () => { this.validateField(name, value) }
+        );
+    }
+
+    validateField(fieldName, value){
+        let fieldValidationErrors = this.state.formErrors;
+
+        let nameValid = this.state.nameValid;
+        let emailValid = this.state.emailValid;
+        let loginValid = this.state.loginValid;
+        let passwordValid = this.state.passwordValid;
+        let passwordConfirmValid = this.state.passwordConfirmValid;
+
+        switch(fieldName){
+            case 'name':
+                nameValid = value.length >= 4;
+                fieldValidationErrors.name = nameValid ? '' : 'Минимальная длина - 4';
+                break;
+            case 'email':
+                emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i) || value.length === 0;
+                fieldValidationErrors.email = emailValid ? '' : 'Неверный формат';
+                break;
+            case 'login':
+                loginValid = value.length >= 4;
+                fieldValidationErrors.login = loginValid ? '' : 'Минимальная длина - 4';
+                break;
+            case 'password':
+                passwordValid = value.length >= 6;
+                fieldValidationErrors.password = passwordValid ? '' : 'Минимальная длина - 6';
+                break;
+            case 'passwordConfirm':
+                passwordConfirmValid = value === this.state.password;
+                fieldValidationErrors.passwordConfirm = passwordConfirmValid ? '' : 'Пароли не совпадают';
+                break;
+            default:
+                break;
+        }
+        this.setState({
+            formErrors: fieldValidationErrors,
+            nameValid: nameValid,
+            emailValid: emailValid,
+            loginValid: loginValid,
+            passwordValid: passwordValid,
+            passwordConfirmValid: passwordConfirmValid
+          }, this.validateForm);
+    }
+
+    validateForm() {
+        this.setState({
+            formValid: 
+            this.state.nameValid &&
+            this.state.emailValid &&
+            this.state.loginValid &&
+            this.state.passwordValid &&
+            this.state.passwordConfirmValid
+        });
+    }
+
+    render(){
+        const errorBaner = this.state.errorMessage ? 
+        <Alert>
+            {this.state.errorMessage}
+        </Alert> : null;
+        
+        return(
+            <>
+            {errorBaner}
+            <div className="form signUp">
+                <h2>Registration</h2>
+                <div className="formContent">
+                    <div className="formGroup">
+                        <label>Username</label>
+                        <input required type="text" name="name" value={this.state.name} onChange={this.handleinputChange}/>
+                        <div className="formFeedback">{this.state.formErrors.name}</div>
+                    </div>
+                    <div className="formGroup">
+                        <label>Email</label>
+                        <input type="email" name="email" value={this.state.email} onChange={this.handleinputChange}/>
+                        <div className="formFeedback">{this.state.formErrors.email}</div>
+                    </div>
+                    <div className="formGroup">
+                        <label>Login</label>
+                        <input required type="text" name="login" value={this.state.login} onChange={this.handleinputChange}/>
+                        <div className="formFeedback">{this.state.formErrors.login}</div>
+                    </div>
+                    <div className="formGroup">
+                        <label>Password</label>
+                        <input required type="password" name="password" id="password" value={this.state.password} onChange={this.handleinputChange}/>
+                        <div className="formFeedback">{this.state.formErrors.password}</div>
+                    </div>
+                    <div className="formGroup">
+                        <label>Confirm password</label>
+                        <input required type="password" name="passwordConfirm" id="passwordConfirm" value={this.state.passwordConfirm} onChange={this.handleinputChange}/>
+                        <div className="formFeedback">{this.state.formErrors.passwordConfirm}</div>
+                    </div>
+                    <Button className="signUpButton" disabled = {!this.state.formValid} onClick={() => this.register()}>Sign Up</Button>
+                </div>
+            </div>
+            </>
+        )
+    }
+
+    register(){
+        if (!this.state.formValid)
+        {
+            this.setState({
+                errorMessage: 'Form is not valid'
+            })
+            return;
+        }
+        let data = {
+            userName: this.state.name,
+            email: this.state.email,
+            login: this.state.login, 
+            password: this.state.password, 
+            passwordConfirm: this.state.passwordConfirm
+        };
+
+        fetch('api/User', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            },
+            body: JSON.stringify(data)
+        }).then((response) => {
+            if (response.ok){
+                this.props.history.push("/signUp");
+            }
+            else {
+                this.setState({error: true});
+                return response.json();
+            }
+        }).then((data) => {
+            if(this.state.error)
+            {
+                this.setState({
+                    errorMessage: data.message
+                });
+            }
+        }).catch((ex) => {
+            this.setState({
+                errorMessage: ex.toString()
+            });
+        });
+    }
+}
