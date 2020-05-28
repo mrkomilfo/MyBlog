cf88d604808361c20484fcd1135dc9c3bd86a975
mrkomilfo <43483871+mrkomilfo@users.noreply.github.com>
2020-05-28 17:26:54 +0300

    DEPLOY WORK!!!

--- MyBlog.Web/Startup.cs
+++ MyBlog.Web/Startup.cs
@@ -35,7 +35,7 @@ namespace MyBlog.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllersWithViews();
 
             services.AddSpaStaticFiles(configuration =>
             {
